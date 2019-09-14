fbbfb8f099e5e3a812f542abc0c5eb1687b61d31
Jeannelou Turbanos <jeanturbanos@gmail.com>
2019-09-13 23:17:48 -0400

    Added Application Layer - Product App Service

---
+++ FMPI.Sales.Application/Products/IProductAppService.cs
@@ -0,0 +1,10 @@
+﻿using FMPI.Sales.Application.Products.Dto;
+using System.Collections.Generic;
+
+namespace FMPI.Sales.Application.Products
+{
+    public interface IProductAppService
+    {
+        List<ProductOutputDto> GetProducts();
+    }
+}
